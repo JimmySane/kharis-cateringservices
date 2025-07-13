f37b167bcbe4d3c9df4c21ecf5b0e76e90f4010d
JIMMY <jsanefu@yahoo.com>
2025-07-13 16:09:55 +0000

    Add GitHub Actions workflow for Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// Detect if we're in production mode
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  base: isProd ? "/kharis-cateringservices/" : "/", // GitHub Pages base path for prod
+  base: "/kharis-cateringservices/",
   plugins: [react()],
   resolve: {
     alias: {
